fab0085a37567e70367c465f934e0b3c298a8e50
izzydream250-dotcom <izzydream250@gmail.com>
2026-03-26 16:36:10 -0700

    Create main plugin class for Minecraft Server

---
+++ src/main/java/com/izzydream250/minecraftserver/MinecraftServer.java
@@ -0,0 +1,15 @@
+package com.izzydream250.minecraftserver;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MinecraftServer extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        // Code to execute when the plugin is enabled
+    }
+
+    @Override
+    public void onDisable() {
+        // Code to execute when the plugin is disabled
+    }
+}
